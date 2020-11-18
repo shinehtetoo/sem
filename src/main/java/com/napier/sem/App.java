19e638ee12c2d7bb918631a74dcc44bdfdea2373
Shine Htet Oo <40478643@live.napier.ac.uk>
2020-11-18 13:07:11 +0630

    Test MySQL Connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,7 +26,7 @@ public class App
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(3000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
